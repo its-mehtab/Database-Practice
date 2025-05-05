4149bbc9275cd7878526b798c6453877301d3f4e
mehtab-CodeCLouds <mehtabalam0908@gmail.com>
2025-05-05 20:07:55 +0530

    add error handling

--- 03Travel Tracker/index.js
+++ 03Travel Tracker/index.js
@@ -22,6 +22,18 @@ async function checkVisited() {
   });
 
   return visitedCountries;
+  // try {
+  //   let visitedCountries = [];
+  //   const res = await db.query("SELECT country_code from visited_countries");
+
+  //   res.rows.forEach((currCountry) => {
+  //     visitedCountries.push(currCountry.country_code);
+  //   });
+
+  //   return visitedCountries;
+  // } catch (err) {
+  //   console.error(err);
+  // }
 }
 
 const app = express();
@@ -43,25 +55,40 @@ app.post("/add", async (req, res) => {
   console.log(newCountry);
   const visitedCountries = await checkVisited();
 
-  const result = await db.query(
-    "SELECT * FROM countries WHERE LOWER(country_name) = LOWER($1)",
-    [newCountry]
-  );
-  console.log(result.rows[0].country_code);
+  try {
+    const result = await db.query(
+      "SELECT * FROM countries WHERE LOWER(country_name) = LOWER($1)",
+      [newCountry]
+    );
+    if (result.rows.length === 0)
+      throw new Error("Country name does not exist, try again.");
 
-  if (
-    !visitedCountries.includes(
-      result.rows[0].country_code || result.rows.length !== 0
-    )
-  ) {
+    if (visitedCountries.includes(result.rows[0].country_code)) {
+      throw new Error("Country has already been added, try again.");
+    }
+
     db.query("INSERT INTO visited_countries (country_code) VALUES($1) ", [
       result.rows[0].country_code,
     ]);
+
+    res.redirect("/");
+  } catch (error) {
+    console.log(error);
+    res.render("index.ejs", {
+      countries: visitedCountries,
+      total: visitedCountries.length,
+      error: error.message,
+    });
   }
+  // if (result.rows.length !== 0) {
+  //   db.query("INSERT INTO visited_countries (country_code) VALUES($1) ", [
+  //     result.rows[0].country_code,
+  //   ]);
+  // } else if (visitedCountries.includes(result.rows[0].country_code)) {
+  //   console.log("dfg");
+  // }
 
   // db.end();
-
-  res.redirect("/");
 });
 
 app.listen(port, () => {
