1e8efa43d6977f6f3c20d82ad8d6f111221794cc
mehtab-CodeCLouds <mehtabalam0908@gmail.com>
2025-05-05 17:54:07 +0530

    work on the post route

--- 03Travel Tracker/index.js
+++ 03Travel Tracker/index.js
@@ -12,21 +12,16 @@ const db = new pg.Client({
   password: process.env.DATABASE_PASSWORD,
 });
 
-let visitedCountries = [];
-
 db.connect();
-
-try {
+async function checkVisited() {
+  let visitedCountries = [];
   const res = await db.query("SELECT country_code from visited_countries");
 
   res.rows.forEach((currCountry) => {
     visitedCountries.push(currCountry.country_code);
   });
-  console.log(visitedCountries);
-} catch (err) {
-  console.error(err);
-} finally {
-  db.end();
+
+  return visitedCountries;
 }
 
 const app = express();
@@ -36,10 +31,37 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get("/", async (req, res) => {
+  const visitedCountries = await checkVisited();
   res.render("index.ejs", {
     countries: visitedCountries,
     total: visitedCountries.length,
   });
+});
+
+app.post("/add", async (req, res) => {
+  const newCountry = req.body.country;
+  console.log(newCountry);
+  const visitedCountries = await checkVisited();
+
+  const result = await db.query(
+    "SELECT * FROM countries WHERE LOWER(country_name) = LOWER($1)",
+    [newCountry]
+  );
+  console.log(result.rows[0].country_code);
+
+  if (
+    !visitedCountries.includes(
+      result.rows[0].country_code || result.rows.length !== 0
+    )
+  ) {
+    db.query("INSERT INTO visited_countries (country_code) VALUES($1) ", [
+      result.rows[0].country_code,
+    ]);
+  }
+
+  // db.end();
+
+  res.redirect("/");
 });
 
 app.listen(port, () => {
