711974a6350b5fd831a3d3bbb543f788ab81b803
mehtab-CodeCLouds <mehtabalam0908@gmail.com>
2025-05-29 19:53:22 +0530

    complete project

--- 05Permalist Project/index.js
+++ 05Permalist Project/index.js
@@ -1,8 +1,20 @@
 import express from "express";
 import bodyParser from "body-parser";
+import pg from "pg";
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
-const port = 3000;
+const port = 4000;
+
+const db = new pg.Client({
+  user: "postgres",
+  password: process.env.DATABASE_PASSWORD,
+  host: "localhost",
+  port: 5432,
+  database: "permalist",
+});
+db.connect();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
@@ -12,7 +24,23 @@ let items = [
   { id: 2, title: "Finish homework" },
 ];
 
-app.get("/", (req, res) => {
+// async function getItems() {
+//   try {
+//     const res = await db.query("SELECT * FROM items");
+//     items = res.rows;
+//   } catch (err) {
+//     console.error(err);
+//   }
+// }
+
+app.get("/", async (req, res) => {
+  // getItems();
+  try {
+    const res = await db.query("SELECT * FROM items");
+    items = res.rows;
+  } catch (err) {
+    console.error(err);
+  }
   res.render("index.ejs", {
     listTitle: "Today",
     listItems: items,
@@ -21,13 +49,26 @@ app.get("/", (req, res) => {
 
 app.post("/add", (req, res) => {
   const item = req.body.newItem;
-  items.push({ title: item });
+  // items.push({ title: item });
+  db.query("INSERT INTO items (title) VALUES ($1)", [item]);
   res.redirect("/");
 });
 
-app.post("/edit", (req, res) => {});
+app.post("/edit", (req, res) => {
+  const { updatedItemId, updatedItemTitle } = req.body;
+  db.query("UPDATE items SET title = $1 WHERE id = $2", [
+    updatedItemTitle,
+    updatedItemId,
+  ]);
+  res.redirect("/");
+});
 
-app.post("/delete", (req, res) => {});
+app.post("/delete", (req, res) => {
+  const id = req.body.deleteItemId;
+
+  db.query("DELETE FROM items WHERE id = $1", [id]);
+  res.redirect("/");
+});
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
